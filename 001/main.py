fa328f8c00d2de512d60982217e4fed2b9cec542
Guilherme Brito <118216080+guiyugi@users.noreply.github.com>
2023-08-30 16:59:58 -0300

    Listas Lineares - 001
    Modificações feitas em sala de aula.

--- 001/main.py
+++ 001/main.py
@@ -1,5 +1,19 @@
 import lib.list_seq as list_seq
 lista1 = list_seq.Lista()
+user_input = 1
+def menu():
+    print('\tEDITOR DE LISTAS')
+    print()
+    print('\t1 - EXIBIR LISTA')
+    print('\t2 - INSERIR')
+    print('\t3 - REMOVER')
+    print('\t4 - EXIBIR ELEMENTO')
+    print('\t5 - EXIBIR POSIÇÃO')
+    print('\t6 - ESVAZIAR')
+    print('     ESC - SAIR')
+    print()
+    user_input = input('    DIGITE SUA OPÇÃO: ')d
+    return user_input
 
 def mostrar_lista():
     global lista1
@@ -11,20 +25,39 @@ def esvaziar_lista():
     print('Lista esvaziada com sucesso')
     return(lista1)
 
-user_input = 3
 while user_input!= 0:
-    print()
-    print('\tEDITOR DE LISTAS')
-    print()
-    print('\t1 - EXIBIR LISTA')
-    print('\t2 - INSERIR')
-    print('\t3 - REMOVER')
-    print('\t4 - EXIBIR ELEMENTO')
-    print('\t5 - EXIBIR POSIÇÃO')
-    print('\t6 - ESVAZIAR')
-    print('ESC - SAIR')
-    print()
-    user_input = int(input('DIGITE UMA OPCAO '))
+    user_input = menu()
+
+    if user_input == '1':
+        print('='*40)
+        mostrar_lista()
+        print('='*40)
+
+    elif user_input == '2':
+        value = int(input('Digite o valor do elemento a ser inserido: '))
+        index = int(input('Digite a posição onde será efetuada a inserção: '))
+        lista1.insert(value,index)
+
+    elif user_input == '3':
+        index = int(input('Insira a a posição do elemento a  ser removido: '))
+        lista1.remover(index)
+
+    elif user_input == '4':
+        index = int(input('Insira a posição do elemento, em seguida será mostrado seu índice: '))
+        print(lista1.show_value(index))
+
+    elif user_input == '5':
+        value = int(input('Insira o valor do elemento, em seguida será mostrada sua posição: '))
+        print(lista1.show_index(value))
+
+    elif user_input == '6':
+        print('='*40)    
+        lista1 = esvaziar_lista()
+        print('SUCESSO!!!')
+
+
+
+""""
     match user_input:
         case 1:
             print('='*40)
@@ -48,3 +81,5 @@ while user_input!= 0:
             lista1 = esvaziar_lista()
             print('='*40)
             print('SUCESSO!!!')
+
+"""
